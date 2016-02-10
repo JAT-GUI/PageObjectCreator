0f9644a3c0ca0dfacb6cec18d7a68321d919d545
Alcides Ticlla <alcidesticlla@cloud.upwork.com>
2016-02-10 01:14:55 -0400

    adds my file

---
+++ src/Bussines/MyFile.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template MyFile, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Bussines;
+
+/**
+ *
+ * @author Brayam Machicado
+ */
+public interface MyFile {
+    String getType() ;
+    void setType(String type);
+    public String getName() ;
+    public void setName(String name); 
+    public String getHeader();
+    public void setHeader(String header);
+    public String getBody();
+    public void setBody(String body) ;    
+    String build_line(String name_property,String target, String type, boolean key, boolean button, boolean select);    
+    public String getFooter() ;
+    public void setFooter(String footer) ;
+    public String absoluteName();
+}
